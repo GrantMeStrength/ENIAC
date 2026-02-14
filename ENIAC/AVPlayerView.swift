9fddd1617ad62042440a9d356e459f7bd0ea7b15
John Kennedy <john_kenn@mac.com>
2026-02-13 18:51:48 -0800

    Initial Commit

---
+++ ENIAC/AVPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  AVPlayerView.swift
+//  ENIAC
+//
+//  Created by John Kennedy on 2/13/26.
+//
+
+import SwiftUI
+
+struct AVPlayerView: UIViewControllerRepresentable {
+    let viewModel: AVPlayerViewModel
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return viewModel.makePlayerViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // Update the AVPlayerViewController as needed
+    }
+}
---
+++ ENIAC/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ENIAC
+//
+//  Created by John Kennedy on 2/13/26.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
